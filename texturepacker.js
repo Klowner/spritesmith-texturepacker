d59f55d00f6a514e4895009010678846b575f716
Peter Groß <me@peter-gross.org>
2020-12-21 18:33:12 +0100

    Add animation data to exported JSON (#1)
    * Add animations to json

--- texturepacker.js
+++ texturepacker.js
@@ -2,7 +2,8 @@ function texturePackerTemplate (params) {
 	var items = params.items,
 		itemObj = { frames: {} },
 		frames = itemObj.frames,
-		item;
+		item,
+		animations = {};
 
 	if (items.length > 0) {
 		item = items[0];
@@ -39,7 +40,22 @@ function texturePackerTemplate (params) {
 				h: item.height
 			}
 		};
+
+		var matches = item.name.match(/(.*?)_\d\d/);
+
+		if (matches) {
+			var animationName = matches[1];
+			var currentAnimations = animations[animationName];
+			var fileName = matches[0];
+			if (!currentAnimations) {
+				animations[animationName] = [fileName];
+			} else {
+				animations[animationName].push(fileName);
+			}
+		}
 	});
+
+	itemObj.animations = animations;
 
 	return JSON.stringify(itemObj, null, 4);
 }
